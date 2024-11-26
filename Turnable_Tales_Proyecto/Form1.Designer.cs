7b3df4d978f80beb64a6447e6ed34fe355d3d90a
Georgina Calzada <gina.hada@gmail.com>
2024-11-26 08:31:45 -0600

    qwert

--- Turnable_Tales_Proyecto/Form1.Designer.cs
+++ Turnable_Tales_Proyecto/Form1.Designer.cs
@@ -29,9 +29,6 @@
         private void InitializeComponent()
         {
             button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
             SuspendLayout();
             // 
             // button1
@@ -43,41 +40,11 @@
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
-            // button2
-            // 
-            button2.Location = new Point(348, 266);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 29);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(348, 112);
-            button3.Name = "button3";
-            button3.Size = new Size(94, 29);
-            button3.TabIndex = 2;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            button4.Location = new Point(348, 327);
-            button4.Name = "button4";
-            button4.Size = new Size(94, 29);
-            button4.TabIndex = 3;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
@@ -88,8 +55,5 @@
         #endregion
 
         private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
     }
 }
