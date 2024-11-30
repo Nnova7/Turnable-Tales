190b97bda104429faad278946f0715eaaf5fea65
Elia <delgadolia76@gmail.com>
2024-11-29 22:25:25 -0600

    FormListado agregado

---
+++ Turnable_Tales_Proyecto/FormListado.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Turnable_Tales_Proyecto
+{
+    partial class FormListado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormListado));
+            buttonRegresar = new Button();
+            listView1 = new ListView();
+            buttonMusica = new Button();
+            buttonUsuario = new Button();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label1 = new Label();
+            labelLinea = new Label();
+            labelTitulo = new Label();
+            labelFrase = new Label();
+            labelMenu = new Label();
+            button1 = new Button();
+            imageListDiscos = new ImageList(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // buttonRegresar
+            // 
+            buttonRegresar.FlatStyle = FlatStyle.Popup;
+            buttonRegresar.Image = (Image)resources.GetObject("buttonRegresar.Image");
+            buttonRegresar.Location = new Point(20, 325);
+            buttonRegresar.Name = "buttonRegresar";
+            buttonRegresar.Size = new Size(82, 97);
+            buttonRegresar.TabIndex = 61;
+            buttonRegresar.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            listView1.BackColor = SystemColors.ActiveCaptionText;
+            listView1.Font = new Font("Segoe UI Historic", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            listView1.ForeColor = SystemColors.ButtonHighlight;
+            listView1.Location = new Point(369, 254);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(627, 338);
+            listView1.TabIndex = 60;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // buttonMusica
+            // 
+            buttonMusica.FlatStyle = FlatStyle.Popup;
+            buttonMusica.Image = (Image)resources.GetObject("buttonMusica.Image");
+            buttonMusica.Location = new Point(1069, 18);
+            buttonMusica.Name = "buttonMusica";
+            buttonMusica.Size = new Size(105, 102);
+            buttonMusica.TabIndex = 59;
+            buttonMusica.UseVisualStyleBackColor = true;
+            // 
+            // buttonUsuario
+            // 
+            buttonUsuario.FlatStyle = FlatStyle.Popup;
+            buttonUsuario.Location = new Point(1159, 18);
+            buttonUsuario.Name = "buttonUsuario";
+            buttonUsuario.Size = new Size(105, 102);
+            buttonUsuario.TabIndex = 54;
+            buttonUsuario.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImageLayout = ImageLayout.None;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 9);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(181, 168);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 55;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(453, 104);
+            label2.Name = "label2";
+            label2.Size = new Size(455, 50);
+            label2.TabIndex = 58;
+            label2.Text = "A D M I N I S T R A D O R\r\n";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.White;
+            label1.Font = new Font("Consolas", 1.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(207, 163);
+            label1.Name = "label1";
+            label1.Size = new Size(893, 3);
+            label1.TabIndex = 57;
+            label1.Text = resources.GetString("label1.Text");
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // labelLinea
+            // 
+            labelLinea.AutoSize = true;
+            labelLinea.BackColor = Color.White;
+            labelLinea.Font = new Font("Consolas", 1.2F, FontStyle.Regular, GraphicsUnit.Point);
+            labelLinea.ForeColor = Color.White;
+            labelLinea.Location = new Point(207, 90);
+            labelLinea.Name = "labelLinea";
+            labelLinea.Size = new Size(893, 3);
+            labelLinea.TabIndex = 56;
+            labelLinea.Text = resources.GetString("labelLinea.Text");
+            labelLinea.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // labelTitulo
+            // 
+            labelTitulo.AutoSize = true;
+            labelTitulo.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point);
+            labelTitulo.ForeColor = Color.White;
+            labelTitulo.Location = new Point(432, 9);
+            labelTitulo.Name = "labelTitulo";
+            labelTitulo.Size = new Size(493, 50);
+            labelTitulo.TabIndex = 52;
+            labelTitulo.Text = "T U R N T A B L E   T A L E S";
+            labelTitulo.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // labelFrase
+            // 
+            labelFrase.AutoSize = true;
+            labelFrase.BackColor = Color.Transparent;
+            labelFrase.ForeColor = Color.White;
+            labelFrase.Location = new Point(567, 59);
+            labelFrase.Name = "labelFrase";
+            labelFrase.Size = new Size(237, 20);
+            labelFrase.TabIndex = 53;
+            labelFrase.Text = "\"TU VIDA, TU MUSICA, TU VINILO.\"\r\n";
+            // 
+            // labelMenu
+            // 
+            labelMenu.AutoSize = true;
+            labelMenu.BackColor = Color.Transparent;
+            labelMenu.Font = new Font("Consolas", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            labelMenu.ForeColor = Color.White;
+            labelMenu.Location = new Point(336, 186);
+            labelMenu.Name = "labelMenu";
+            labelMenu.Size = new Size(703, 36);
+            labelMenu.TabIndex = 51;
+            labelMenu.Text = "LISTADO DE DISCOS ORDENADOS POR EXISTENCIAS";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Image = Properties.Resources.Imagen1_removebg_preview;
+            button1.Location = new Point(1159, 25);
+            button1.Margin = new Padding(2);
+            button1.Name = "button1";
+            button1.Size = new Size(97, 89);
+            button1.TabIndex = 191;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // imageListDiscos
+            // 
+            imageListDiscos.ColorDepth = ColorDepth.Depth8Bit;
+            imageListDiscos.ImageSize = new Size(16, 16);
+            imageListDiscos.TransparentColor = Color.Transparent;
+            // 
+            // FormListado
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(1302, 633);
+            Controls.Add(button1);
+            Controls.Add(buttonRegresar);
+            Controls.Add(listView1);
+            Controls.Add(buttonMusica);
+            Controls.Add(buttonUsuario);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(labelLinea);
+            Controls.Add(labelTitulo);
+            Controls.Add(labelFrase);
+            Controls.Add(labelMenu);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FormListado";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonRegresar;
+        private ListView listView1;
+        private Button buttonMusica;
+        private Button buttonUsuario;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label1;
+        private Label labelLinea;
+        private Label labelTitulo;
+        private Label labelFrase;
+        private Label labelMenu;
+        private Button button1;
+        private ImageList imageListDiscos;
+    }
+}
