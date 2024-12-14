9e860cf0e4510cc89af5d75caa6bca7986fa36f0
Nnova7 <gina.hada@gmail.com>
2024-12-13 21:52:59 -0600

    MEjor na

--- Turnable_Tales_Proyecto/AudioPlayer.cs
+++ Turnable_Tales_Proyecto/AudioPlayer.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 
-namespace Turnable_Tales_Proyecto
+public static void ToggleMusic()
 {
-    internal class AudioPlayer
+    string musicFilePath = @"Resources\music.wav"; // Ruta del archivo de música
+    Console.WriteLine("Ruta del archivo: " + System.IO.Path.GetFullPath(musicFilePath)); // Mostrar ruta completa
+
+    try
     {
+        if (isPlaying)
+        {
+            Console.WriteLine("Pausando la música...");
+            player.Stop();
+            isPlaying = false;
+        }
+        else
+        {
+            Console.WriteLine("Reproduciendo la música...");
+            player = new SoundPlayer(musicFilePath); // Cargar el archivo de música
+            player.PlayLooping();  // Reproducir en loop
+            isPlaying = true;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error al reproducir la música: " + ex.Message);
     }
 }
